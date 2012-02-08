1aa4bea4d7f5ffa32acdd02c1a9cf7269d5d52f2
Vurt Yan <yanyilin224@gmail.com>
2012-02-08 21:09:03 +0800

    添加通用资源bundle
    Signed-off-by: Vurt Yan <yanyilin224@gmail.com>

---
+++ nms-core-common/src/me/vurt/nms/core/common/Activator.java
@@ -0,0 +1,40 @@
+package me.vurt.nms.core.common;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext context) throws Exception {
+		InputStream is=Activator.class.getResourceAsStream("/conf/log4j.properties");
+		if(is!=null){
+			Properties props = new Properties();
+			props.load(is);
+
+			PropertyConfigurator.configure(props);
+			
+			Logger logger=LoggerFactory.getLogger(Activator.class);
+			logger.info("logger started!");
+		}else{
+			System.err.println("没有找到日志配置文件");
+		}
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		LogManager.shutdown();
+	}
+
+}
